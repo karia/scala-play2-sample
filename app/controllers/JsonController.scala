5712037f1bcd8084722c8b65863c14b4994cd5cf
karia <karia@side2.net>
2015-10-27 17:59:40 +0900

    implemented create,delete,edit methods

---
+++ app/controllers/JsonController.scala
@@ -0,0 +1,42 @@
+package controllers
+
+/**
+ * Created by karia on 2015/10/27.
+ */
+
+import play.api.mvc._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.db.slick._
+import slick.driver.JdbcProfile
+import models.Tables._
+import javax.inject.Inject
+import scala.concurrent.Future
+import slick.driver.H2Driver.api._
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
+  with HasDatabaseConfigProvider[JdbcProfile] {
+
+  /**
+   * 一覧表示
+   */
+  def list = TODO
+
+  /**
+   * ユーザ登録
+   */
+  def create = TODO
+
+  /**
+   * ユーザ更新
+   */
+  def update = TODO
+
+  /**
+   * ユーザ削除
+   */
+  def remove(id: Long) = TODO
+}
